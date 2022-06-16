b7a3df095128662d6773578e86915885e6c8d5fd
S-moreo <78574820+S-moreo@users.noreply.github.com>
2022-06-16 15:20:42 -0400

    BetterBehavior Identification

---
+++ src/main/java/net/smoreo/betterbehavior/BetterBehavior.java
@@ -0,0 +1,15 @@
+package net.smoreo.betterbehavior;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BetterBehavior implements ModInitializer {
+	public static final String MOD_ID = "betterbehavior";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/smoreo/betterbehavior/BetterBehaviorClient.java
@@ -0,0 +1,10 @@
+package net.smoreo.betterbehavior;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterBehaviorClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
