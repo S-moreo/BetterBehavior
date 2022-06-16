840b176060d490970ad280078ce7ea0a72030d09
S-moreo <78574820+S-moreo@users.noreply.github.com>
2022-06-16 15:22:16 -0400

    Lets get cooking!

--- src/main/java/net/smoreo/betterbehavior/BetterBehavior.java
+++ src/main/java/net/smoreo/betterbehavior/BetterBehavior.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+//Lets get cooking!
 public class BetterBehavior implements ModInitializer {
 	public static final String MOD_ID = "betterbehavior";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
